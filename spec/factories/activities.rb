fb67d709c992ea76ff9c97034022d4976afc8142
riosoftware <riosoftwareau@gmail.com>
2015-03-08 11:55:11 +1100

    Formatting code.

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
+
   factory :activity do
     subject "MyString"
-comments "MyString"
-due_date "2015-03-04"
-priority 1
-status 1
-contact nil
+    comments "MyString"
+    due_date "2015-03-04"
+    priority 1
+    status 1
+    contact nil
   end
 
 end
